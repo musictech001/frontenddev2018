0a6bf81ffea04ce31413122fdc2105ef33e2cacb
musictech001 <musictech001@gmail.com>
2018-07-17 16:52:29 -0400

    display items using <input> element.

--- 2018-07-21-assign-3/Todo/src/Components/ShowItem.js
+++ 2018-07-21-assign-3/Todo/src/Components/ShowItem.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react'
 
 export default class ShowItem extends Component {
 	constructor(props){
-		super(props)
-
+		super(props);
+		this.state = {list:this.props.list};
+		console.log("ShowItem()");
 	}
 
+
+
 	render() {
+		console.log("ShowItem.render()");
+		const list = this.state.list;
 		return(
 			<div>
-				<div>Show Item.</div>
-				<h3>{this.props.list[0].name}</h3>
+				<div>Todo list</div>
+
+				<div>
+					<ol>
+					    {list.map((item) => {
+	                		return <li key={item.name + '-li'}> 
+	                			<input type="text" key={item.name + '-name'} defaultValue={item.name}/>
+	                			<input type="text" key={item.name + '-prio'} defaultValue={item.priority}/>
+	                		</li>
+	              		})}
+              		</ol>
+				</div>				
 			</div>
 		)
 	}
