eceb28954ae11a1bf1116855bb4aba021d3b268e
musictech001 <musictech001@gmail.com>
2018-07-18 17:50:24 -0400

    tic-tac-toe: time travel via stepNumber

--- 2018-07-21-assign-3/tic-tac-toe/src/index.js
+++ 2018-07-21-assign-3/tic-tac-toe/src/index.js
@@ -86,13 +86,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),        
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick = (i)=>{
     console.log("i=" + i);
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = history[history.length -1];
     const squares = current.squares.slice();
     // check winner
@@ -104,21 +105,29 @@ class Game extends React.Component {
       history: history.concat([{
         squares : squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step) {
+    console.log("jumpto step "+step);
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ===0,
+    })
+  }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ?
         'Go to move #' + move : 'Go to game start';
         return (
-          <li> <button onClick={()=>this.jumpTo(move)}>
+          <li key={move}> <button onClick={()=>this.jumpTo(move)}>
             {desc}
           </button> </li>
           );
