84ace833c9613e0801af6fcfc4d951a6e8f14491
musictech001 <musictech001@gmail.com>
2018-07-22 10:55:41 -0400

    tic-tac-toe: clean up.

--- 2018-07-21-assign-3/tic-tac-toe/src/index.js
+++ 2018-07-21-assign-3/tic-tac-toe/src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button 
-//         className="square" 
-//         onClick={()=>this.props.onClick()}
-//       >
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
-
 // functional component (stateless)
 function Square (props) {
   return (
